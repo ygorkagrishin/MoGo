5a590156b88a548e7184314ce3d79945e2ae325e
ygorkagrishin <webygorkagrishin@gmail.com>
2017-10-05 22:26:23 +0300

    iss017

--- app/js/carousel.js
+++ app/js/carousel.js
@@ -1,6 +1,27 @@
 "use strict";
 
+function Timer( callback, delay ) {
+
+var that = this, timerID, start, remaining = delay;
+that.resume = function () {
+    
+        start = new Date();
+        timerID = setTimeout( function () {
+
+        that.resume();
+        callback();
+    
+        }, remaining);
+
+    }
+
+that.resume();
+
+}
+
 function Carousel( settings ) {
+
+var that = this;
 
 var privates = {};
 
@@ -25,54 +46,71 @@ privates.position = {
 privates.direction = {
 
     'current_directX' : 0,
-    'current_directY' : 0,
-    'final_directX' : 0,
-    'final_directY' : 0
+    'final_directX' : 0
 
 }
 
-// Prev
+privates.default = {
 
-privates.prev_slide = function () {
+    'autoplay' : true,
+    'autoplay_delay' : 3000
+
+}
+
+that.prev_slide = function () {
 
     --privates.position.current_position;
 
-    if ( privates.position.current_position < 0 ) {
-    privates.sel.wrap.classList.add( 'test-carousel__no-active' );    
-    privates.position.current_position = privates.position.max_position;
-    setTimeout( function (){
+    privates.sel.wrap.style.transform = 'translateX(-' + privates.position.current_position + '00%)';
 
-    privates.sel.wrap.classList.remove( 'test-carousel__no-active' );    
+    if ( privates.position.current_position < 0 ) {
+      
+    privates.sel.wrap.style.transform = 'translateX(100%)';
+    
+    setTimeout( function () {
+
+    privates.sel.wrap.classList.add( 'test-carousel__no-active' );     
+    privates.position.current_position = privates.position.max_position;
+    privates.sel.wrap.style.transform = 'translateX(-' + privates.position.current_position + '00%)';
+
+    setTimeout( function () {
+    privates.sel.wrap.classList.remove( 'test-carousel__no-active' );     
     privates.position.current_position = privates.position.max_position - 1;
     privates.sel.wrap.style.transform = 'translateX(-' + privates.position.current_position + '00%)';
 
-    }, 20);  }
-    
-    privates.sel.wrap.style.transform = 'translateX(-' + privates.position.current_position + '00%)';
+            }, 20);
 
-    }; 
+        }, 500)
 
-// Next
+    }
 
-privates.next_slide = function () {
+}
+
+that.next_slide = function () {
 
     ++privates.position.current_position;
 
-    if ( privates.position.current_position > privates.position.max_position - 1) {
-
-    setTimeout( function () {
-    
-    privates.position.current_position = 0;
-    privates.sel.wrap.classList.add( 'test-carousel__no-active' );
-    privates.sel.wrap.style.transform = 'translateX(' + privates.position.current_position + '00%)';
-
-    }, 500); }
-    privates.sel.wrap.classList.remove( 'test-carousel__no-active' );
     privates.sel.wrap.style.transform = 'translateX(-' + privates.position.current_position + '00%)';
 
-    };
+    if ( privates.position.current_position >= privates.position.max_position ) {
 
-// Swipe
+    setTimeout( function () {
+
+        privates.sel.wrap.classList.add( 'test-carousel__no-active' );
+        privates.position.current_position = 0;    
+        privates.sel.wrap.style.transform = 'translateX(100%)';
+
+        setTimeout( function () {
+
+        privates.sel.wrap.classList.remove( 'test-carousel__no-active' ); 
+        privates.sel.wrap.style.transform = 'translateX(' + privates.position.current_position + '00%)';
+
+            }, 20);
+
+        }, 500);
+
+    }
+}
 
 privates.sel.carousel.addEventListener( 'touchstart', function (e) {
     e.preventDefault();
@@ -87,13 +125,12 @@ privates.sel.carousel.addEventListener( 'touchend', function (e) {
     privates.direction.final_directX = e.changedTouches[0].screenX, privates.direction.final_directY = e.changedTouches[0].screenY;
 
     privates.direction.current_directX > privates.direction.final_directX ?
-    privates.next_slide() : privates.prev_slide();
+    that.next_slide() : that.prev_slide();
 
 });
 
-if ( privates.sel.prev !== undefined ) privates.sel.prev.addEventListener( 'click', privates.prev_slide);
-
-if ( privates.sel.prev !== undefined ) privates.sel.next.addEventListener( 'click', privates.next_slide);
+privates.sel.prev.addEventListener( 'click', that.prev_slide);
+privates.sel.next.addEventListener( 'click', that.next_slide);
 
 };
 
